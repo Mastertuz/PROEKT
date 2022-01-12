b2b51bee4bb6313320e9568371a4ffb63d95c620
Mastertuz <89520195+Mastertuz@users.noreply.github.com>
2022-01-12 18:44:09 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -3,6 +3,13 @@ $(function(){
     $('.header-top').toggleClass
     ('header-top--active');
    });
+
+   $(".menu a, .go-top").on("click", function (event) {
+		event.preventDefault();
+		var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1000);
+	});
 });
 
 
